0b863ce57f4863a1cb78da2a1af8f7755c3d05be
lookwaii <waii_piya@hotmail.com>
2020-01-01 15:31:41 +0700

    Exercise4_Piyabud_H

---
+++ Exercise4_Piyabud_H.py
@@ -0,0 +1,9 @@
+FE = 79.5
+GB = 65
+ICS = 75
+CP = 50
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business : ",GB)
+print("Introduction to Computer System :",ICS)
+print("computer programming :",CP)
